0dbf3bb372530f8268591fc63618643a0fa5c859
Muhamed Zahiri <105281690+tnt-muhamedz@users.noreply.github.com>
2022-05-11 14:00:24 +0200

    Initial Commit

---
+++ Team Picker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team Picker
+//
+//  Created by Muhamed Zahiri on 11.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
